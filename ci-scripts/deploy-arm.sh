d3558edaafa66e39f01b6ffe87902ad3b855d175
Maykel Arias Torres <dev0006@simelo.tech>
2019-07-17 02:31:23 -0400

    [ci-script] Repair bad definition ENV in deploy-arm.sh

--- ci-scripts/deploy-arm.sh
+++ ci-scripts/deploy-arm.sh
@@ -6,7 +6,7 @@ git --version
 
 export VERSION="$(git describe --tags --exact-match HEAD)"
 
-echo $(QEMU_PLATFORM)
+echo $QEMU_PLATFORM
 
 if [[ "$VERSION" ]]; then
 	docker build  --build-arg SHA1=$CIRCLE_SHA1 --build-arg GITHUB_OAUTH_TOKEN --build-arg PROJECT_USERNAME=$CIRCLE_PROJECT_USERNAME --build-arg PROJECT_REPONAME=$CIRCLE_PROJECT_REPONAME --build-arg QEMU_PLATFORM --build-arg VERSION  --file $GOPATH/src/github.com/skycoin/libskycoin/docker/images/deploy-arm/Dockerfile  $GOPATH/src/github.com/skycoin/libskycoin -t skydev-deploy
