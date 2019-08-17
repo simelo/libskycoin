9b68f750077ff3b8d1ad6f4832147de7412554dd
Maykel Arias Torres <dev0006@simelo.tech>
2019-08-17 11:43:10 -0400

    [libc][test] refs #105 Finalized test `TestMaxChildDepthError`

--- lib/cgo/tests/check_cipher.bip32.bip32.c
+++ lib/cgo/tests/check_cipher.bip32.bip32.c
@@ -35,11 +35,37 @@ typedef struct {
 } testMasterKey;
 
 
-START_TEST(TestBip32TestVectors)
+START_TEST(TestMaxChildDepthError)
 {
+    GoUint8 bufferTemp[1024];
+    GoSlice tmp = {bufferTemp, 0, 32};
+    randBytes(&tmp, 32);
+    PrivateKey__Handle key = 0;
+    GoUint32 err = SKY_bip32_NewMasterKey(tmp, &key);
+    ck_assert_int_eq(err, SKY_OK);
+    GoUint8 reached = 0;
+    for (size_t i = 0; i < 256; i++) {
+        err = SKY_bip32_Private_NewPrivateChildKey(key, 0, &key);
+        switch (i) {
+        case 255:
+            ck_assert_int_eq(err, SKY_ERROR);
+            reached = 1;
+            break;
+
+        default:
+            ck_assert_int_eq(err, SKY_OK);
+            break;
+        }
+    }
+    ck_assert(reached == 1);
 }
 END_TEST
 
+START_TEST(TestImpossibleChildError){
+
+
+
+}END_TEST
 Suite* cipher_bip32(void)
 {
     Suite* s = suite_create("Load cipher.bip32");
@@ -47,7 +73,8 @@ Suite* cipher_bip32(void)
 
     tc = tcase_create("cipher.bip32");
     tcase_add_checked_fixture(tc, setup, teardown);
-    tcase_add_test(tc, TestBip32TestVectors);
+    tcase_add_test(tc, TestMaxChildDepthError);
+    tcase_add_test(tc, TestImpossibleChildError);
     suite_add_tcase(s, tc);
     tcase_set_timeout(tc, 150);
 
