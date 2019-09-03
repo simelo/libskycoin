60cec497af9386a510e5fe87daf460da1751ce1e
Maykel Arias Torres <dev0006@simelo.tech>
2019-09-02 21:17:54 -0400

    [cgo] refs #105 Repair function `base58`

--- lib/cgo/cipher.base58.base58.go
+++ lib/cgo/cipher.base58.base58.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	"encoding/hex"
-	"reflect"
 	"unsafe"
 
 	"github.com/skycoin/skycoin/src/cipher/base58"
@@ -18,40 +17,38 @@ import (
 import "C"
 
 //export SKY_base58_Hex2Base58
-func SKY_base58_Hex2Base58(_val []byte, _arg1 *C.GoString_) (____error_code uint32) {
+func SKY_base58_Hex2Base58(_val []byte, _arg1 *string) (____error_code uint32) {
 	val := *(*[]byte)(unsafe.Pointer(&_val))
-	__arg1 := string(base58.Encode(val))
-	copyString(__arg1, _arg1)
+	*_arg1 = string(base58.Encode(val))
 	return
 }
 
 //export SKY_base58_Encode
-func SKY_base58_Encode(_bin []byte, _arg1 *C.GoString_) (____error_code uint32) {
+func SKY_base58_Encode(_bin []byte, _arg1 *string) (____error_code uint32) {
 	bin := *(*[]byte)(unsafe.Pointer(&_bin))
-	__arg1 := base58.Encode(bin)
-	copyString(__arg1, _arg1)
+	*_arg1 = base58.Encode(bin)
+
 	return
 }
 
 //export SKY_base58_Decode
-func SKY_base58_Decode(_s string, _arg1 *C.GoSlice_) (____error_code uint32) {
+func SKY_base58_Decode(_s string, _arg1 *[]byte) (____error_code uint32) {
 	s := _s
 	__arg1, ____return_err := base58.Decode(s)
 	____error_code = libErrorCode(____return_err)
 	if ____return_err == nil {
-		copyToGoSlice(reflect.ValueOf(__arg1), _arg1)
+		*_arg1 = __arg1
 	}
 
 	return
 }
 
 //export SKY_base58_String2Hex
-func SKY_base58_String2Hex(_s string, _arg1 *C.GoSlice_) (____error_code uint32) {
-	s := _s
-	__arg1, ____return_err := hex.DecodeString(s)
+func SKY_base58_String2Hex(_s string, _arg1 *[]byte) (____error_code uint32) {
+	__arg1, ____return_err := hex.DecodeString(_s)
 	____error_code = libErrorCode(____return_err)
 	if ____return_err == nil {
-		copyToGoSlice(reflect.ValueOf(__arg1), _arg1)
+		*_arg1 = __arg1
 	}
 
 	return
