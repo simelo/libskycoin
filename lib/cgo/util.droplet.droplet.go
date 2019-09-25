e34fbf6c7fdfe525501c86cf2a736954324af838
Maykel Arias Torres <dev0006@simelo.tech>
2019-09-25 00:20:21 -0400

    [cgo] refs #105 Correcting memory `util.droplet`

--- lib/cgo/util.droplet.droplet.go
+++ lib/cgo/util.droplet.droplet.go
@@ -23,12 +23,12 @@ func SKY_droplet_FromString(_b string, _arg1 *uint64) (____error_code uint32) {
 }
 
 //export SKY_droplet_ToString
-func SKY_droplet_ToString(_n uint64, _arg1 *string) (____error_code uint32) {
+func SKY_droplet_ToString(_n uint64, _arg1 *C.GoString_) (____error_code uint32) {
 	n := _n
 	__arg1, ____return_err := droplet.ToString(n)
 	____error_code = libErrorCode(____return_err)
 	if ____return_err == nil {
-		*_arg1 = __arg1
+		copyString(__arg1, _arg1)
 	}
 	return
 }
