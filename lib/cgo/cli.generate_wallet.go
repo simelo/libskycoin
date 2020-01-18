834f0bd42db2a66ae80a0c3db18ef1614bc83a1f
Maykel Arias Torres <dev0006@simelo.tech>
2020-01-17 23:32:12 -0500

    [cgo] refs #116 Finalized `cli.generate_wallet`

--- lib/cgo/cli.generate_wallet.go
+++ lib/cgo/cli.generate_wallet.go
@@ -1,8 +1,6 @@
 package main
 
-import (
-	cli "github.com/SkycoinProject/skycoin/src/cli"
-)
+import cli "github.com/SkycoinProject/skycoin/src/cli"
 
 /*
 
@@ -10,11 +8,13 @@ import (
   #include <stdlib.h>
 
   #include "skytypes.h"
+  #include "skyfee.h"
 */
 import "C"
 
 //export SKY_cli_MakeAlphanumericSeed
-func SKY_cli_MakeAlphanumericSeed(_arg0 *string) (____error_code uint32) {
-	*_arg0 = cli.MakeAlphanumericSeed()
+func SKY_cli_MakeAlphanumericSeed(_arg0 *C.GoString_) (____error_code uint32) {
+	__arg0 := cli.MakeAlphanumericSeed()
+	copyString(__arg0, _arg0)
 	return
 }
