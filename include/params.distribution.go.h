6588240034c9b0088ed3344c90481ad6a7705cc4
Maykel Arias Torres <dev0006@simelo.tech>
2020-01-17 23:28:37 -0500

    [cgo] refs #116 Finalized `params.distribution`

---
+++ include/params.distribution.go.h
@@ -0,0 +1,8 @@
+typedef struct{
+    GoUint64_ MaxCoinSupply;
+    GoUint64_ InitialUnlockedCount;
+    GoUint64_ UnlockAddressRate;
+    GoUint64_ UnlockTimeInterval;
+    GoSlice_  Addresses;
+    GoSlice_  addressesDecoded;
+} params__Distribution;
