e7628aa16c41a2d863ac11b77cdea6a254883c7f
Maykel Arias Torres <dev0006@simelo.tech>
2020-01-17 23:24:22 -0500

    [cgo] refs #116 Finalized `cipher.bip39`

--- lib/cgo/cipher.bip39.bip39.go
+++ lib/cgo/cipher.bip39.bip39.go
@@ -11,39 +11,64 @@ import (
 
   #include <string.h>
   #include <stdlib.h>
+  #include <stdbool.h>
 
   #include "skytypes.h"
+  #include "skyfee.h"
 */
 import "C"
 
-//export SKY_bip39_NewDefaultMnemomic
-func SKY_bip39_NewDefaultMnemomic(_arg0 *string) (____error_code uint32) {
+//export SKY_bip39_NewDefaultMnemonic
+func SKY_bip39_NewDefaultMnemonic(_arg0 *C.GoString_) (____error_code uint32) {
 	__arg0, ____return_err := bip39.NewDefaultMnemonic()
 	____error_code = libErrorCode(____return_err)
 	if ____return_err == nil {
-		*_arg0 = __arg0
+		copyString(__arg0, _arg0)
 	}
 	return
 }
 
 //export SKY_bip39_NewEntropy
-func SKY_bip39_NewEntropy(_bitSize int, _arg1 *[]byte) (____error_code uint32) {
+func SKY_bip39_NewEntropy(_bitSize int, _arg1 *C.GoSlice_) (____error_code uint32) {
 	bitSize := _bitSize
 	__arg1, ____return_err := bip39.NewEntropy(bitSize)
 	____error_code = libErrorCode(____return_err)
 	if ____return_err == nil {
-		*_arg1 = __arg1
+		copyToGoSlice(reflect.ValueOf(__arg1), _arg1)
+	}
+	return
+}
+
+//export SKY_bip39_EntropyFromMnemonic
+func SKY_bip39_EntropyFromMnemonic(_mnemonic string, _arg1 *C.GoSlice_) (____error_code uint32) {
+	mnemonic := _mnemonic
+	__arg1, ____return_err := bip39.EntropyFromMnemonic(mnemonic)
+	____error_code = libErrorCode(____return_err)
+	if ____return_err == nil {
+		copyToGoSlice(reflect.ValueOf(__arg1), _arg1)
 	}
 	return
 }
 
 //export SKY_bip39_NewMnemonic
-func SKY_bip39_NewMnemonic(_entropy []byte, _arg1 *string) (____error_code uint32) {
+func SKY_bip39_NewMnemonic(_entropy []byte, _arg1 *C.GoString_) (____error_code uint32) {
 	entropy := *(*[]byte)(unsafe.Pointer(&_entropy))
 	__arg1, ____return_err := bip39.NewMnemonic(entropy)
 	____error_code = libErrorCode(____return_err)
 	if ____return_err == nil {
-		*_arg1 = __arg1
+		copyString(__arg1, _arg1)
+	}
+	return
+}
+
+//export SKY_bip39_NewSeed
+func SKY_bip39_NewSeed(_mnemonic string, _password string, _arg2 *C.GoSlice_) (____error_code uint32) {
+	mnemonic := _mnemonic
+	password := _password
+	__arg2, ____return_err := bip39.NewSeed(mnemonic, password)
+	____error_code = libErrorCode(____return_err)
+	if ____return_err == nil {
+		copyToGoSlice(reflect.ValueOf(__arg2), _arg2)
 	}
 	return
 }
@@ -53,17 +78,7 @@ func SKY_bip39_ValidateMnemonic(_mnemonic string) (____error_code uint32) {
 	mnemonic := _mnemonic
 	____return_err := bip39.ValidateMnemonic(mnemonic)
 	____error_code = libErrorCode(____return_err)
-	return
-}
-
-//export SKY_bip39_NewSeed
-func SKY_bip39_NewSeed(_mnemonic string, _password string, _arg1 *C.GoSlice_) (____error_code uint32) {
-	mnemonic := _mnemonic
-	password := _password
-	__arg1, ____return_err := bip39.NewSeed(mnemonic, password)
-	____error_code = libErrorCode(____return_err)
 	if ____return_err == nil {
-		copyToGoSlice(reflect.ValueOf(__arg1), _arg1)
 	}
 	return
 }
